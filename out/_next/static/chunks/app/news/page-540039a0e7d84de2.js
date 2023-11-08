e906045e20401ef91b99bcf3acdfb7241f2fd93d
Linus-f <70658257+Linus-f@users.noreply.github.com>
2023-11-08 18:55:25 +0000

    Update main to output generated at cde4c27

---
+++ out/_next/static/chunks/app/news/page-540039a0e7d84de2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[834],{8980:function(e,n,s){Promise.resolve().then(s.bind(s,8661)),Promise.resolve().then(s.t.bind(s,3492,23))}},function(e){e.O(0,[661,492,195,338,744],function(){return e(e.s=8980)}),_N_E=e.O()}]);
